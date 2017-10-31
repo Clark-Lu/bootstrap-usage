4a4d5f5002b3c3b804e29d203bcd28f09d26916e
Clark-Lu <31913218+Clark-Lu@users.noreply.github.com>
2017-10-31 21:59:02 +0800

    thymeleaf and bootstrap

--- src/main/java/com/luchang/bootstrapusage/controller/HomeController.java
+++ src/main/java/com/luchang/bootstrapusage/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String home(){
-        return "home/index";
+        return "index";
     }
 }
